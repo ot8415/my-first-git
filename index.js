c937947a7297a34c0b7b3382bf19a349a9e90621
user-PC\user <ot8415@naver.com>
2016-12-12 13:33:35 +0900

    버튼 기능 추가

---
+++ index.js
@@ -0,0 +1,10 @@
+var btn = document.getElementById("login");
+var reg = document.getElementById("regist");
+
+btn.addEventListener('click',function(){
+    alert('로그인 버튼을 클릭 했습니다.');
+});
+
+reg.addEventListener('click',function(){
+    alert('회원가입 버튼을 클릭 했습니다.');
+});

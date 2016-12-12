52267a3a7e3393448129fc72955265b780d751e6
otherUser <ot8415@naver.com>
2016-12-12 15:01:40 +0900

    스크립트 이벤트 변

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ var btn = document.getElementById("login");
 var reg = document.getElementById("regist");
 
 btn.addEventListener('click',function(){
-    alert('로그인 버튼을 클릭 했습니다.');
+    var userId = document.getElementById("userId").value;
+    var userPw = document.getElementById("userPw").value;
+    if(userId !="" && userPw !=""){
+        alert("로그인에 성공했습니다.");
+    }else{
+        alert("빈 값을 채워 주세요.");
+    }
 });
 
 reg.addEventListener('click',function(){
